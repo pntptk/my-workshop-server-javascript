ba3062ec6c63b318bad08f1733bfe49273342454
Pladniti Phomtakai <167268137+ITiti123npc1npg@users.noreply.github.com>
2026-02-15 23:13:56 +0700

    CRUD-backend

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ let todos = [
     startDay: "2026-01-12",
     endDay: "2026-01-12",
     status: "",
-    duration:5,
+    duration: 5,
   },
 ];
 
@@ -28,10 +28,10 @@ app.get("/todos", (req, res) => {
 app.post("/todos", (req, res) => {
   const { name, description, status, startDay, endDay } = req.body;
 
-    if (!name) {
-      res.status(400).json({message : "Name is required"});
-      return;
-    }
+  if (!name) {
+    res.status(400).json({ message: "Name is required" });
+    return;
+  }
   const newTodo = {
     id: Date.now(),
     name,
@@ -39,18 +39,40 @@ app.post("/todos", (req, res) => {
     status,
     startDay,
     endDay,
-    duration:5,
+    duration: 5,
   };
 
   todos.push(newTodo);
   res.status(201).json(todos);
 });
 
+app.delete("/todos/:id", (req, res) => {
+  const id = req.params.id;
 
-app.delete("/todos/:id",(req,res)=>{
-  const id = req.params.id
+  const initialLength = todos.length;
 
-})
+  todos = todos.filter((t) => t.id !== parseInt(id));
+
+  if (todos.length === initialLength) {
+    return res.status(404).json({ message: "ไม่พบข้อมูลที่ต้องการ" });
+  }
+
+  res.status(200).json({ message: "ลบสำเร็จ", todos });
+});
+
+app.put("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  const { name, description, status } = req.body;
+
+  const index = todos.findIndex((t) => t.id === parseInt(id));
+
+  if (index !== -1) {
+    todos[index] = { ...todos[index], name, description, status };
+    return res.status(200).json({ message: "update data", todos });
+  } else {
+    return res.status(404).json({message : "not found id",todos});
+  }
+});
 // const server = createServer(app);
 
 app.listen(3001, () => {
